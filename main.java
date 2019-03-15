c348e2f55438da15378f56ab36fe95c48803c741
Your Name <siddhrathyadav1999@gmail.com>
2019-03-15 10:48:29 +0530

    initial code

---
+++ main.java
@@ -0,0 +1,130 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+class Java_task{
+	
+    static Scanner sc = new Scanner(System.in);
+    String[] student;
+    String[][] list,qu;
+    int[] taken_action;
+    float[] cgpa;
+    Java_task(){
+	
+	}
+	void take_input()
+	{
+		int test_case=Integer.parseInt(sc.nextLine());
+		student=new String[test_case];
+        list=new String[test_case][];
+        taken_action=new int[test_case];
+        cgpa=new float[test_case];
+        qu=new String[0][0];
+		for(int i=0;i<test_case;i++){
+            student[i]=sc.nextLine();
+		}
+        for(int i=0;i<test_case;i++){
+            list[i]=student[i].split(" ");
+            cgpa[i]=-1;
+            if(list[i][0].equals("ENTER")){
+                taken_action[i]=0;//for ENTER event
+            }
+            else{
+                taken_action[i]=1;//for SERVED event
+            }
+        }
+        
+    }
+   
+	private String[][] store(String[][] array, String[] store) {
+        String[][] longer = new String[array.length + 1][];
+        for (int i = 0; i < array.length; i++){
+            longer[i] = array[i];
+        }
+        longer[array.length] = store;
+        return longer;
+    }
+	
+	
+	
+    private String[][] retrieve(String[][] array){
+        if(array.length == 0){
+            return array;
+        }
+        else{
+            String[][] longer = new String[array.length - 1][];
+            for (int i = 1; i < array.length; i++){
+                longer[i-1] = array[i];
+            }
+            return longer;
+        }
+    }
+
+   void calculate(){
+        int j=0;
+        for(int i=0;i<taken_action.length;i++){
+            if(taken_action[i]==0){
+                qu=store(qu,list[i]);
+            }
+            else{
+                qu=priority(qu);
+                qu=retrieve(qu);
+            }
+        }
+   }
+   void display()
+   {   
+	 if(qu.length == 0){
+            System.out.println("EMPTY");
+        }
+        else{
+        	qu=priority(qu);
+            for(int i=0;i<qu.length;i++ ){
+                System.out.println(qu[i][1]);
+            }
+        }
+    }
+   
+
+   String[][] priority(String[][] array){
+        String[] n1,n2;
+        n2=new String[2];
+        float num1,num2;
+        for(int i=0;i<array.length-1;i++){
+            for(int j=0;j<array.length-i-1;j++){
+                num1=Float.parseFloat(array[j][2]);
+                num2=Float.parseFloat(array[j+1][2]);
+                if(num1<num2){
+                    n1=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=n1;
+                }
+                else if(num1==num2){
+                    n2[0]=array[j][1];
+                    n2[1]=array[j+1][1];
+                    if(n2[0].equals(n2[1])){
+                        int tm1=Integer.parseInt(array[j][3]);
+                        int tm2=Integer.parseInt(array[j+1][3]);
+                        if(tm1>tm2){
+                            n1=array[j];
+                            array[j]=array[j+1];
+                            array[j+1]=n1;
+                        }
+                    }
+                    else{
+                        String tm=n2[1];
+                        List tls;
+                        Arrays.sort(n2);
+                        if(n2[0].equals(tm)){
+                            n1=array[j];
+                            array[j]=array[j+1];
+                            array[j+1]=n1;
+                        }
+                    }
+                }
+            }
+        }
+        return qu;
+    }
+    
+    
+}
---
+++ test.java
@@ -0,0 +1,13 @@
+public class test{
+
+	public static void main(String[] args) {
+        Java_task task1 = new Java_task();
+		System.out.println("Taking Input...\n");
+		task1.take_input();
+		System.out.println("\nCalculating...\n");
+		task1.calculate();
+		System.out.println("Calculation Complete.\nDisplaying Result...\n");
+		task1.display();
+    }
+	
+}
